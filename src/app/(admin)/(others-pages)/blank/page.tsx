68988359a6bc9662a6005c4cfa291b5045a2e875
Junior Santos <juniior.pvh@gmail.com>
2025-03-23 17:37:56 -0400

    first commit

---
+++ src/app/(admin)/(others-pages)/blank/page.tsx
@@ -0,0 +1,27 @@
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Next.js Blank Page | TailAdmin - Next.js Dashboard Template",
+  description: "This is Next.js Blank Page TailAdmin Dashboard Template",
+};
+
+export default function BlankPage() {
+  return (
+    <div>
+      <PageBreadcrumb pageTitle="Blank Page" />
+      <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12">
+        <div className="mx-auto w-full max-w-[630px] text-center">
+          <h3 className="mb-4 font-semibold text-gray-800 text-theme-xl dark:text-white/90 sm:text-2xl">
+            Card Title Here
+          </h3>
+          <p className="text-sm text-gray-500 dark:text-gray-400 sm:text-base">
+            Comece a colocar conteúdo em grades ou painéis, você também pode usar diferentes combinações de grades.
+            Por favor, confira o painel e outras páginas.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
